9bd623f22d161ba9913e1675a45fa404ec7c95bd
Miguel <machavezg@icloud.com>
2014-12-15 15:29:57 -0800

    Update Lottery.java

--- Lottery.java
+++ Lottery.java
@@ -2,15 +2,18 @@ import java.util.Scanner;
 import java.util.Arrays;
 import java.lang.String;
 import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 // Use this to call out word utils
 // import org.apache.commons.lang.WordUtils;
 import static java.lang.System.out;
 
 class Lottery {
   public static void main(String args[]) throws IOException {
-    String [] LotteryTypes = { "powerball", "mega millions", "super lotto", "fantasy 5", "daily 4", "daily 3", "daily derby", "hot spot"};
+    String[] LotteryTypes = { "powerball", "mega millions", "super lotto", "fantasy 5", "daily 4", "daily 3", "daily derby", "hot spot"};
     Scanner lotteryScanner = new Scanner(System.in);
-    String input, lower;
+    String input, lower, newStr;
     StringBuilder sb = new StringBuilder();
 //    out.println(LotteryTypes[0-1]);
     out.print("What lottery numbers do you want generated? ");
@@ -26,51 +29,44 @@ class Lottery {
         // The code below is used to store the value of lower and use it outside the loop statement.
         sb.append(lower);
     }
-    out.print("Here are your numbers!\n");
-    out.println(lower);
+//    if (Arrays.asList(LotteryTypes[0]).contains(lower)) {
+//      String lottery = LotteryTypes[0];
+//      out.println(lottery);
+//    }
+    if (lower.equals(LotteryTypes[0])) {
+      out.print("You Chose Powerball");
+    }
+    else if (lower.equals(LotteryTypes[1])) {
+//     out.println(Arrays.toString(Arrays.copyOfRange(LotteryTypes, 1, 8)));
+     out.println("You Chose Mega Millions");
+     // out.print(Arrays.toString(newAr));
+    }
+    else if (lower.equals(LotteryTypes[2])) {
+     out.println("You Chose Super Lotto");
+    }
+    else if (lower.equals(LotteryTypes[3])) {
+     out.println("You Chose Fantasy 5");
+    }
+    else if (lower.equals(LotteryTypes[4])) {
+     out.println("You Chose Daily 4");
+    }
+    else if (lower.equals(LotteryTypes[5])) {
+     out.println("You Chose Daily 3");
+    }
+    else if (lower.equals(LotteryTypes[6])) {
+     out.println("You Chose Daily Derby");
+    }
+    else {
+     out.println("You Chose Hot Spot");
+    }
+//    BufferedReader br = new BufferedReader(new FileReader("powerball.txt"));
+//    String line;
+//    while ((line = br.readLine()) != null) {
+//      out.println(line);
+//    }
+//    br.close();
+//        
+//    out.print("Here are your numbers!\n");
+//    out.println(lower);
   }
 }
-// Check This out
-// http://stackoverflow.com/questions/19653120/access-value-outside-while-loop
-// This code is used to capitalize but wont work with DrJava
-//      WordUtils.capitalize("You chose");
-//      out.print("Please pick from Powerball, MegaMillions, Super Lotto, Fantasy 5, Daily 4, Daily 3, Daily Derby or Hot Spot: ");
-//      input = lotteryScanner.nextLine();
-//      lower = input.toLowerCase();
-//      
-//      if (Arrays.asList(LotteryTypes).contains(lower2)){
-//        out.println("Thank You For Playing");
-//      }
-//    }
-// I could've used tons of if or else if statements and end up with a large code.
-//    if (lower.equals(LotteryTypes[0])) {
-//      out.println(input);
-//    }
-//    else if (lower.equals(LotteryTypes[1])) {
-//      out.println(input);
-//    }
-//    else if (lower.equals(LotteryTypes[2])) {
-//      out.println(input);
-//    }
-//    else if (lower.equals(LotteryTypes[3])) {
-//      out.println(input);
-//    }
-//    else if (lower.equals(LotteryTypes[4])) {
-//      out.println(input);
-//    }
-//    else if (lower.equals(LotteryTypes[5])) {
-//      out.println(input);
-//    }
-//    else if (lower.equals(LotteryTypes[6])) {
-//      out.println(input);
-//    }
-//    else if (lower.equals(LotteryTypes[7])) {
-//      out.println(input);
-//    }
-//    else {
-//      out.println("Invalid Input");
-//    }
-//              
-//    out.print(LotteryTypes[0]);
-//    out.println();
-//    
