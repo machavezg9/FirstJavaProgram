0772be00d23ee2e9b93ac2faa29b38ecdd7633a8
Miguel <machavezg@icloud.com>
2014-12-20 17:50:59 -0800

    Update LotteryScannerPB.java

--- LotteryScannerPB.java
+++ LotteryScannerPB.java
@@ -24,7 +24,8 @@ class LotteryScannerPB {
             String line = br.readLine();
                 String tmp[] = line.split("[ ., ]+");
                 draw = Integer.parseInt(tmp[0]);
-                date = tmp[1]; month = tmp[2]; 
+                date = tmp[1]; 
+                month = tmp[2]; 
                 day = Integer.parseInt(tmp[3]);
                 year = Integer.parseInt(tmp[4]);
                 firNum = Integer.parseInt(tmp[5]); secNum = Integer.parseInt(tmp[6]); thNum = Integer.parseInt(tmp[7]);
